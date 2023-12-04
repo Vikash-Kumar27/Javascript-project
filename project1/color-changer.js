ee84274ec3177958d3768c5db50043f9096e6b17
Vikash Kumar <152673507+Vikash-Kumar27@users.noreply.github.com>
2023-12-04 14:52:31 +0530

    Update color-changer.js

--- project1/color-changer.js
+++ project1/color-changer.js
@@ -4,16 +4,16 @@ const body = document.querySelector("body")
 button.forEach(element => {
     element.addEventListener("click",function(e){
         if(e.target.id==="grey"){
-            document.body.style.backgroundColor=e.target.id
+           body.style.backgroundColor=e.target.id
         }
         if(e.target.id==="white"){
-            document.body.style.backgroundColor=e.target.id
+           body.style.backgroundColor=e.target.id
         }
         if(e.target.id==="blue"){
-            document.body.style.backgroundColor=e.target.id
+            body.style.backgroundColor=e.target.id
         }
         if(e.target.id==="yellow"){
-            document.body.style.backgroundColor=e.target.id
+            body.style.backgroundColor=e.target.id
         }
         
     })
