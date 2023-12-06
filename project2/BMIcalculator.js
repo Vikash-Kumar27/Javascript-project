499ad6d9395c74b05e9b36aa3037ea912a79cad5
Vikash Kumar <152673507+Vikash-Kumar27@users.noreply.github.com>
2023-12-06 12:26:14 +0530

    Update BMIcalculator.js

--- project2/BMIcalculator.js
+++ project2/BMIcalculator.js
@@ -1,10 +1,10 @@
-const form = document.querySelector('form')
+const form = document.querySelector('form');
 
 form.addEventListener("submit",function(e){
     e.preventDefault()
-    const height = parseInt(document.querySelector('#height').value)
-    const weight = parseInt(document.querySelector('#weight').value)
-    const result = document.querySelector('#results')
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#results');
     
     if(height === ""|| height < 0 || isNaN(height)){
         result.innerHTML ="please give a valid Height"
@@ -13,9 +13,10 @@ form.addEventListener("submit",function(e){
        result.innerHTML ="please give a valid Weight"
     }
     else{
-        const BMI = (weight/(height*height/10000)).toFixed(2)
-        if(BMI<=18.6)
+        const BMI = (weight/(height*height/10000)).toFixed(2);
+        if(BMI<=18.6){
         result.innerHTML = `<span>${BMI} BMI <br> Underweight</span> `
+        }
         else if(BMI>18.6 && BMI<=24.9){
         result.innerHTML = `<span> ${BMI} BMI <br> Normal</span> `
         }
@@ -23,7 +24,7 @@ form.addEventListener("submit",function(e){
             result.innerHTML = `<span> ${BMI} BMI <br> Overweight</span> `
         }
 
-    }
+    };
      
    
      
